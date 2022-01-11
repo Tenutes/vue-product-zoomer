8cc7e7e80916e79c34354f8892751a7b1ffb099d
tenutes <leopard19670508>
2022-01-11 13:51:12 +0300

    [FIX] publish

--- src/publish.js
+++ src/publish.js
@@ -1,9 +1,3 @@
 import Zoomer from './components/ProductZoomer'
 
-const ProductZoomer = {
-  install(app) {
-    app.component(Zoomer.name, Zoomer)
-  }
-}
-
-export default ProductZoomer
+export default Zoomer
